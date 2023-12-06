52f591987a937fde3fd6c887a05c40730d3e93b3
Georgios Koukas <geocucas@gmail.com>
2023-12-06 23:43:37 +0200

    trying to ad scorer

--- main/src/test_winner.py
+++ main/src/test_winner.py
@@ -2,103 +2,12 @@ from utils import *
 from trolley_scenario import TrolleyScenario
 pedestrian_data = pd.read_csv('trolley.csv')
         
-def plot_results(results):
-    # Harm Score over Scenarios
-    plt.figure()
-    plt.plot(results['harm_scores'], marker='o')
-    plt.title('Harm Score Over Scenarios')
-    plt.xlabel('Scenario Number')
-    plt.ylabel('Harm Score')
-    plt.grid(True)
-    
-    # Distribution of Harm Scores
-    plt.figure()
-    plt.hist(results['harm_scores'], bins=20)
-    plt.title('Distribution of Harm Scores')
-    plt.xlabel('Harm Score')
-    plt.ylabel('Frequency')
-    plt.grid(True)
-    
-    # Pedestrians Hit / Total Pedestrians
-    ratios = [hit / total for hit, total in zip(results['pedestrians_hit'], results['total_pedestrians'])]
-    plt.figure()
-    plt.plot(ratios, marker='o')
-    plt.title('Ratio of Pedestrians Hit Over Scenarios')
-    plt.xlabel('Scenario Number')
-    plt.ylabel('Pedestrians Hit / Total Pedestrians')
-    plt.grid(True)
-
-    # Average Steering Angle Over Scenarios
-    plt.figure()
-    plt.plot(results['avg_steering'], marker='o')
-    plt.title('Average Steering Angle Over Scenarios')
-    plt.xlabel('Scenario Number')
-    plt.ylabel('Average Steering Angle')
-    plt.grid(True)
-    
-    # Show all plots
-    plt.show()
-
-def compute_average_steering(all_scenario_steering_values):
-    num_scenarios = len(all_scenario_steering_values)
-    num_timesteps = len(all_scenario_steering_values[0])  # Assuming all scenarios have the same number of timesteps
-
-    average_steering_over_time = []
-
-    for t in range(num_timesteps):
-        avg_steer_at_t = sum([scenario[t] for scenario in all_scenario_steering_values]) / num_scenarios
-        average_steering_over_time.append(avg_steer_at_t)
-
-    return average_steering_over_time
-all_scenario_steering_values = []
-
 results_neat_model, results_left_model, results_right_model, results_straight_model = (create_results_dictionary() for _ in range(4))
 
- 
-def plot_average_steering(average_steering):
-    plt.figure()
-    plt.plot(average_steering, marker='o')
-    plt.title('Average Steering Over Time for All Scenarios')
-    plt.xlabel('Time Step')
-    plt.ylabel('Average Steering Value')
-    plt.grid(True)
-    plt.show()
-
-def pad_steering_values(all_scenario_steering_values):
-    max_length = max(len(scenario) for scenario in all_scenario_steering_values)
-    for scenario in all_scenario_steering_values:
-        while len(scenario) < max_length:
-            scenario.append(0)  # Appending the last steering value
-    return all_scenario_steering_values
-
-def control_live_feed(ego):
-    fig, axs = plt.subplots(3, 1) 
-    while True:
-        if ego:
-            control = ego.get_control()
-            brake = control.brake
-            steering = control.steer
-            throttle = control.throttle
-
-            axs[0].cla()  # Clear the previous plot
-            axs[0].bar(['Throttle'], [throttle])
-            axs[0].set_ylim(0, 1)  # Set y-axis limit for throttle
-
-            axs[1].cla()
-            axs[1].bar(['Brake'], [brake])
-            axs[1].set_ylim(0, 1)  # Set y-axis limit for brake
-
-            axs[2].cla()
-            axs[2].bar(['Steering'], [steering])
-            axs[2].set_ylim(-1, 1)  # Set y-axis limit for steering
-
-if __name__ == "__main__":    
+ if __name__ == "__main__":    
     with open('winner_net.pkl', 'rb') as input_file:
         loaded_winner_net = pickle.load(input_file)
 
-    
-   
-   
     client = carla.Client('localhost', 2000)
     client.set_timeout(15)
     world = client.get_world()
